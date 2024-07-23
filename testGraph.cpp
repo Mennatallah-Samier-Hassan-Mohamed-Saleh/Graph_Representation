a59e2f23956135cd375fa809623d58d086ef36be
Mennatallah-Samier-Hassan-Mohamed-Saleh <mennaallahsamier97@gmail.com>
2024-07-23 11:36:31 +0400

    Update Graph test file and add output

--- testGraph.cpp
+++ testGraph.cpp
@@ -3,14 +3,6 @@
 #include "Graph.cpp"
 using namespace std;
 int main(){
-    int x=0;
-    int y=0;
-    for (int i=0;i<y;++i){
-        x++;
-        cout<<"Meann"<<endl;
-    }
-    std::cout<<x<<endl;
-
     Graph obj("simpleGRaph.adj");
     obj.convertToCSR("outputFullGraph");
 }
